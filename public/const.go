440767f708515d1a84789c9aca869426a6782276
oif <neowiz@icloud.com>
2017-04-09 19:24:01 +0800

    update workdir

--- public/const.go
+++ public/const.go
@@ -3,7 +3,7 @@ package public
 const (
 	APIKey = "7fUJd46QtlmJkBWn"
 	// WorkDir = "/Users/neo/development/go/src/ink/"
-	WorkDir   = "/tiki"
+	WorkDir   = "/tiki/"
 	QueueName = "ink"
 	OriginDir = "origin"
 	ParsedDir = "parsed"
